d66464f4cb0e7da3cc1c7e53badcb02aac2705f2
ColinRoxs <colin.mcp1029@gmail.com>
2026-02-12 18:21:43 -0500

    version 1
    Updated the Attack Script to use the old input system despite my feelings about it. Began work on my part of the anchor functionality.
    
    More work to be done.

--- Assets/Scripts/player/AttackScript.cs
+++ Assets/Scripts/player/AttackScript.cs
@@ -1,30 +1,24 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class AttackScript : MonoBehaviour
 {
     [Header("Charge Settings")]
-    [SerializeField] private float maxChargeTime = 2f;
+    [SerializeField] private float maxChargeTime = 2f; // adjust as nessisary to get the feel of charging right.
     [SerializeField] private float minChargeTime = 0f;
 
     [Header("Projectile")]
-    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject projectilePrefab; //WILL CHANGE, gonna change this to support different projectiles for each Anchor to help with buff usage. Later in code I'll determine which Anchor is being used and switch which is the used projectile.
     [SerializeField] private Transform firePoint;
     [SerializeField] private float fireCooldown = 0.5f;
     private float lastFireTime = -999f;
 
-    private PlayerInputActions input;
     private float chargeTimer;
     private bool isCharging;
+
     [SerializeField] private FrogTongue frogTongue;
 
     private void Awake()
     {
-        input = new PlayerInputActions();
-
-        input.Player.Shoot.started += ctx => StartCharging();
-        input.Player.Shoot.canceled += ctx => ReleaseCharging();
-
         if (frogTongue == null)
         {
             frogTongue = GetComponentInChildren<FrogTongue>();
@@ -38,29 +32,39 @@ public class AttackScript : MonoBehaviour
     private bool CanShoot()
     {
         if (frogTongue == null)
-            return true; 
+            return true;
 
         return !frogTongue.extending && !frogTongue.retracting;
     }
 
-    private void OnEnable()
-    {
-        if (frogTongue != null)
-        {
-            input.Player.Tongue.started += ctx => frogTongue.BeginTongue();
-            input.Player.Tongue.canceled += ctx => frogTongue.EndTongue();
-        }
-
-        input.Enable();
-    }
-
-    private void OnDisable()
-    {
-        input.Disable();
-    }
-
     void Update()
     {
+        // --- SHOOT INPUT ---
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCharging();
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ReleaseCharging();
+        }
+
+        // --- TONGUE INPUT ---
+        if (frogTongue != null)
+        {
+            if (Input.GetButtonDown("Fire2"))
+            {
+                frogTongue.BeginTongue();
+            }
+
+            if (Input.GetButtonUp("Fire2"))
+            {
+                frogTongue.EndTongue();
+            }
+        }
+
+        // --- CHARGING LOGIC ---
         if (isCharging)
         {
             chargeTimer += Time.deltaTime;
@@ -79,9 +83,7 @@ public class AttackScript : MonoBehaviour
     private void ReleaseCharging()
     {
         if (!isCharging) return;
-
         if (!CanShoot()) return;
-
         if (Time.time < lastFireTime + fireCooldown) return;
 
         isCharging = false;
@@ -90,7 +92,7 @@ public class AttackScript : MonoBehaviour
 
         FireProjectile(chargePercent);
 
-        lastFireTime = Time.time; 
+        lastFireTime = Time.time;
     }
 
     private void FireProjectile(float chargePercent)
